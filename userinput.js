6f77aa9ad30249aca8f214042df9b167c99feb50
shristi verma <shristiverma1306@gmail.com>
2023-09-21 10:56:08 +0530

    first commit to js ducat

---
+++ userinput.js
@@ -0,0 +1,15 @@
+// a=prompt("Enter first number:");
+// b=prompt("Enter second number:");
+// document.write("<h2>Addition:</h2>", a+b);
+
+// x=parseInt(prompt("Enter first number:"));
+// y=parseInt(prompt("Enter second number:"));
+// document.write("<h2> Addition:</h2>", x+y);
+
+// p=parseFloat(prompt("Enter first parsefloat number:"));
+// q=parseFloat(prompt("Enter second parsefloat number:"));
+// document.write("<h2>Addition:</h2>", p+q);
+
+// r=eval(prompt("Enter eval number:"));
+// s=eval(prompt("Enter eval 2nd number:"));
+// document.write("<h2> Addition:</h2>", r+s);
